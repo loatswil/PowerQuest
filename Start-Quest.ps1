f907a06d78d45aa79200f1d66d41a1eb872aa249
wal_a <wal_a@home.ku.edu>
2022-10-06 15:52:55 -0700

    Updated interface to menu and dumped Show-Progress cmdlet

--- Start-Quest.ps1
+++ Start-Quest.ps1
@@ -1,6 +1,13 @@
-﻿$Adverbs = @();$Actions = @();$Apps = @();$Objects = @()
+﻿<# 
+ToDo
+    Build Show-Progress that takes int and counts that far...
+    Add alignment into random choice when levelling
+    Add titles based on level
+#>
+
+$Char = @{}
+$Adverbs = @();$Actions = @();$Apps = @();$Objects = @()
 $Mods = @();$Mats = @();$Items = @();$Bonuses = @();$Gear = @()
-$Titles = @{};$Char = @{}
 $Alignments = @()
 $Alignments = @('Lawful Good - White Hat','Neutral Good - Cream Hat','Chaotic Good - Beige Hat',
     'Lawful Neutral - Light-Grey Hat','True Neutral - Grey Hat','Chaotic Neutral - Dark-Grey Hat',
@@ -27,19 +34,19 @@ $Apps = @('Net Neutrality','Big Data','Data Mining','Actionable Analytics','Arti
     'Machine Learning','Personalization','Voice Recognition','Chatbots','Augmented Reality (AR)','Virtual Reality (VR)',
     'Robotics','Smart Industry 4.0','Internet Of Things (IoT)','Quantum Computing','Blockchain','Technological Unemployment',
     'Multi-Factor Authentication','Everything-as-a-Service','PowerBI','Artificial Inteligence','DUO','Blackboard',
-    'Identity Management','Canvas','DNS','Skype for Business','Active Directory','the network','the cloud',
-    'SharePoint','Teams','Azure','the firewall','Bitlocker','Exchange','the portal','the load balancer')
+    'Identity Management','Canvas','DNS','Skype','Active Directory','the network','the cloud',
+    'SharePoint','Teams','Azure','the firewall','Bitlocker','Exchange','the portal')
 $Objects = @('the database','directories','servers','websites','folders','email','users','phone numbers','hard drives',
     'floppy disks','IP Addresses','software','firmware','memory','firewall rules','routing tables','fiber optics','NIC cards',
-    'CAT5 cables','spreadsheets','floppy disks','disk drives')
+    'CAT5 cables','spreadsheets')
 $Mods = @('deadly','magical','digital','electronic','binary',
-    'caffeinated','cloud based','virtual')
+    'caffeinated')
 $Mats = @('golden','platinum','silver','iron','mithril','emerald','ruby',
     'diamond','steel','bronze','copper','saphire')
-$Items = @('goggles','mouse pad','haptic gloves','t-shirt','headset','visor','pocket protector','CAT5 cable',
-    'keyboard','mouse','monitor','USB stick','floppy disk','hard drive')
-$Bonuses = @('of grepping','of editing','of application slaying','of cloud slaying','of database monitoring',
-    'of spreadsheet sorting','of log searching','of portal slaying','of Azure','of event parsing')
+$Items = @('visor','goggles','t-shirt','cell phone','USB stick','hard drive','CAT-5 cable','headset',
+    'keyboard','mouse','monitor','disk','floppy disk')
+$Bonuses = @('of greping','of editing','of application slaying','of Cloud slaying','of database monitoring',
+    'of spreadsheet sorting','of log searching','of portal slaying','of Azure','of ticket closing','of event parsing')
 
 function Randomize-List {
    Param(
@@ -48,42 +55,28 @@ function Randomize-List {
    return $InputList | Get-Random -Count 1
 }
 
-function Get-Align {
-    $Align = Randomize-List -InputList $Alignments
-    Write-Output "$Align"
+function Spin-Up([string]$input_text = 'Press any key to continue... ') {
+    $spin="/-\|"
+    #$spin=".oO0Oo."
+    Write-Host "$input_text" -nonewline
+    $i=0
+    $RandomSpin = Get-Random 15
+    while ($i -le $RandomSpin) {
+        Write-Host "`b$($spin.Substring($i++%$spin.Length)[0])" -nonewline
+        Start-Sleep -Milliseconds 100
+        }
+    #Write-Host ""
 }
 
-function Get-Quest {
-    $Action = Randomize-List -InputList $Actions
-    $App = Randomize-List -InputList $Apps
-    $Action = $Action.substring(0,1).toupper()+$Action.substring(1).tolower()
-    Write-Output "$Action $App..."
-}
-
-function Get-Task {
-    $Adverb = Randomize-List -InputList $Adverbs
-    $Action = Randomize-List -InputList $Actions
-    $Object = Randomize-List -InputList $Objects
-    $Adverb = $Adverb.substring(0,1).toupper()+$Adverb.substring(1).tolower()
-    Write-Output "$Adverb $Action $Object..."
-}
-
-function Say-Level {
-    Add-Type -AssemblyName System.speech
-    $speak = New-Object System.Speech.Synthesis.SpeechSynthesizer
-    $speak.Rate   = 2  # -10 to 10; -10 is slowest, 10 is fastest
-    $level = $Char.level
-    $speak.Speak("Congrats on level $level")
-}
-
-Function Build-Gear {
-    $Mod = Randomize-List -InputList $Mods
-    $Mat = Randomize-List -InputList $Mats
-    $Item = Randomize-List -InputList $Items
-    $Bonus = Randomize-List -InputList $Bonuses
-    $Mod = $Mod.substring(0,1).toupper()+$Mod.substring(1).tolower()
-    $Roll = (Get-Random -Minimum 1 -Maximum 9)
-    Write-Output "$Mod +$Roll $Mat $Item $Bonus"
+function Show-Progress([string]$input_text = 'Press any key to continue... ') {
+    Write-Host "$input_text" -nonewline
+    $i=0
+    $RandomSpin = 2
+    while ($i -le $RandomSpin) {
+        Write-Host "." -NoNewline;$i++
+        Start-Sleep -Milliseconds 100
+        }
+    #Write-Host ""
 }
 
 Function Build-Title {
@@ -100,41 +93,211 @@ Function Build-Title {
     return $Title
 }
 
-Clear-Host
+Function Roll-Stat {
+    $Stats = 1..4 | ForEach-Object {Get-Random -Minimum 1 -Maximum 7}
+    $Smallest = $Stats | Measure-Object -Minimum | Select-Object -ExpandProperty Minimum
+    $Sum = $Stats | Measure-Object -Sum | Select-Object -ExpandProperty Sum
+    $Stat = ($Sum - $Smallest)
+    Write-Output $Stat
+}
 
-while (1) {
-    $Char['name']=($env:username)
-    for ($L = 1; $L -le 100; $L++) {
-        $Char['level']=$L
-        $Title = (Build-Title $L)
-        $Align = (Get-Align)
-        #Say-Level
-        $LevelLoop = @{ID = 0; Activity = "$env:username, Level $L $Title"; Status = "$Align"}
-        Write-Progress @LevelLoop
-        $R = (Get-Random 20)
-        $Exp = (1.5 * $L)
-        for ($E = 1; $E -le $Exp; $E++){
-            if ((Get-Random 100) -le 100) {$Gear = Build-Gear}
-            $GearLoop = @{ID = 3; Activity = "Equipment:"; Status = $Gear}
-            $ExpLoop = @{ID = 4; Activity = "Current level:"; Status = "Experience:"; PercentComplete = $E/$Exp * 100}
-            Write-Progress @GearLoop
-            Write-Progress @Exploop
-            $Quest = (Get-Quest)
-            #[console]::beep(3000,150)
-            $wsh = New-Object -ComObject WScript.Shell
-            $wsh.SendKeys('+{F15}')
-            for($B = 1; $B -le $R; $B++) {
-                $Task = (Get-Task)
-                $QuestLoop = @{ID = 1; Activity = "Current Quest: $Quest"; Status = "Progress->"; PercentComplete = $B/$R * 100}
-                Write-Progress @QuestLoop
-                $R2 = (Get-Random 75)
-                for($A = 1; $A -le $R2; $A++ ) {
-                    $Stat3 = (($A/$R2) * 100).ToString("#")
-                    $TaskLoop = @{ID = 2; Activity = $Task; Status = "Progress-->"; PercentComplete = $A/$R2 * 100}
-                    Write-Progress @TaskLoop
-                    Start-Sleep -Milliseconds (Get-Random 100)
-                }
+function Get-Align {
+    $Align = Randomize-List -InputList $Alignments
+    Write-Output "$Align"
+}
+
+function Get-Quest {
+    $Action = Randomize-List -InputList $Actions
+    $App = Randomize-List -InputList $Apps
+    $Action = $Action.substring(0,1).toupper()+$Action.substring(1).tolower()
+    Write-Output "$Action $App"
+}
+
+function Get-Task {
+    $Adverb = Randomize-List -InputList $Adverbs
+    $Action = Randomize-List -InputList $Actions
+    $Object = Randomize-List -InputList $Objects
+    $Adverb = $Adverb.substring(0,1).toupper()+$Adverb.substring(1).tolower()
+    Write-Output "$Adverb $Action $Object"
+}
+
+Function Build-Gear {
+    $Mod = Randomize-List -InputList $Mods
+    $Mat = Randomize-List -InputList $Mats
+    $Item = Randomize-List -InputList $Items
+    $Bonus = Randomize-List -InputList $Bonuses
+    $Mod = $Mod.substring(0,1).toupper()+$Mod.substring(1).tolower()
+    $Roll = (Get-Random -Minimum 1 -Maximum 9)
+    Write-Output "$Mod +$Roll $Mat $Item $Bonus"
+}
+
+function Choose-Class {
+    param (
+        [string]$Title = 'Choose your class:'
+    )
+    Clear-Host
+    Write-Host "$Title"
+    Write-Host "[D]ruid (default)"
+    Write-Host "[M]age"
+    Write-Host "[W]arrior"
+    Write-Host "[R]anger"
+    Write-Host "M[O]nk"
+    Write-Host "[C]leric"
+    Write-Host "[P]aladin"
+    Write-Host ""
+    $Class = Read-Host "Choose your class"
+    $Class = switch ($Class) {
+    D {'Druid'}
+    M {'Mage'}
+    W {'Warrior'}
+    R {'Ranger'}
+    O {'Monk'}
+    C {'Cleric'}
+    P {'Paladin'}
+    default {'Druid'}
+    }
+    $Char['Class']=$Class
+}
+
+function Choose-Race {
+    param (
+        [string]$Title = 'Choose your race:'
+    )
+    Clear-Host
+    Write-Host "$Title"
+    Write-Host "[H]uman (default)"
+    Write-Host "[E]lf"
+    Write-Host "[D]warf"
+    Write-Host "Hob[B]it"
+    Write-Host "[O]rc"
+    Write-Host "[G]oblin"
+    Write-Host ""
+    $Race = Read-Host "Choose your race"
+    $Race = switch ($Race ) {
+    H {'Human'}
+    E {'Elf'}
+    D {'Dwarf'}
+    B {'Hobbit'}
+    O {'Orc'}
+    G {'Goblin'}
+    default {'Human'}
+    }
+    $Char['Race']=$Race
+}
+
+Function Choose-Name {
+    $Name = Read-Host "Character name?"
+    $Char['Name']=$Name
+}
+
+function Write-Menu {
+    Clear-Host
+    Write-Information "" -InformationAction Continue
+    Write-Information "*********************************" -InformationAction Continue
+    Write-Information "*                               *" -InformationAction Continue
+    Write-Information "* oO[-Welcome to PowerQuest-]Oo *" -InformationAction Continue
+    Write-Information "*                               *" -InformationAction Continue
+    Write-Information "*********************************" -InformationAction Continue
+    Write-Information "" -InformationAction Continue
+}
+
+Function Do-Quest {
+    Param(
+     [int]$CurrentLevel
+    )
+    $Exp = (Get-Random -Minimum 1 -Maximum 5)
+    for($Q = 1; $Q -le $Exp; $Q++) {
+        $Quest = (Get-Quest)
+        Write-Host ""
+        Show-Progress "Current Quest: $Quest"
+        Write-Host "";Write-Host ""
+        $RandomTask=(Get-Random -Minimum 1 -Maximum 5)
+        for($T = 1; $T -le $RandomTask; $T++ ) {
+            $Task = (Get-Task)
+            Show-Progress "$Task"
+            Write-Host ""
+            Start-Sleep -Milliseconds (Get-Random 100)
             }
         }
+    $Exp += $Char['Experience']
+    $Gear = (Build-Gear)
+    $Char['Weapon'] = $Gear
+    $Char['Experience'] = $Exp
+    if ($Exp -ge ($CurrentLevel * 10)){
+    $Char.Level++
     }
 }
+
+Function Get-Stats {
+    Clear-Host
+    $STR = (Roll-Stat)
+    Show-Progress "Rolling STR"
+    Write-Host "";Write-Host "$STR"
+    $Char['str']=$STR
+    $DEX = (Roll-Stat)
+    Show-Progress "Rolling DEX"
+    Write-Host "";Write-Host "$DEX"
+    $Char['dex']=$DEX
+    $CON = (Roll-Stat)
+    Show-Progress "Rolling CON"
+    Write-Host "";Write-Host "CON"
+    $Char['con']=$CON
+    $INT = (Roll-Stat)
+    Show-Progress "Rolling INT"
+    Write-Host "";Write-Host "$INT"
+    $Char['int']=$INT
+    $WIS = (Roll-Stat)
+    Show-Progress "Rolling WIS"
+    Write-Host "";Write-Host "$WIS"
+    $Char['wis']=$WIS
+    $CHA = (Roll-Stat)
+    Show-Progress "Rolling CHA"
+    Write-Host "";Write-Host "$CHA"
+    $Char['cha']=$CHA
+    Pause
+}
+
+Function Main {
+    Clear-Host
+    $Name=($Char.name);$Level=($Char.level);$Race=($Char.race)
+    $Class=($Char.class);$STR=($Char.str);$Con=($Char.con);
+    $INT=($Char.int);$DEX=($Char.dex);$WIS=($Char.wis);$CHA=($Char.cha);
+    $Weapon=($Char.weapon)
+    $Title = (Build-Title $Level)
+    Write-Host "======================================================================"
+    Write-Host "|"
+    Write-Host "| Welcome $Name, level $Level $Title"
+    Write-Host "| $Race $Class"
+    Write-Host "| Current Weapon: $Weapon"
+    Write-Host "|"
+    Write-Host "| Stats:"
+    Write-Host "| Strength: $STR"
+    Write-Host "| Dexterity: $DEX"
+    Write-Host "| Constitution: $CON"
+    Write-Host "| Intelligence: $INT"
+    Write-Host "| Wisdom: $WIS"
+    Write-Host "| Charisma: $CHA"
+    Write-Host "|"
+    Write-Host "======================================================================"
+    Do-Quest $Level
+}
+
+Write-Menu
+Choose-Class
+Choose-Race
+Get-Stats
+
+if (!($Char.level)){$Char.level = 1}
+if (!($Char.name)){$Char.name = "Wilgrin"}
+#if (!($Name)){$Name = "Gwendoveer"}
+
+$Char.Add('Weapon', $Gear)
+$Char.Add('Alignment', $Alignment)
+
+do {
+    Main
+    Start-Sleep 5
+    $wsh = New-Object -ComObject WScript.Shell
+    $wsh.SendKeys('+{F15}')
+}
+until ($Cont -eq 'n')
